61002eb7fdce46b50261eb2213f06b97885e328f
Asim Aslam <asim@aslam.me>
2019-05-22 13:37:35 +0000

    working version

--- handler/handler.go
+++ handler/handler.go
@@ -2,41 +2,119 @@ package handler
 
 import (
 	"context"
-	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
-	"time"
 
-	"github.com/micro/go-micro/client"
-	stream "path/to/service/proto/stream"
+	"github.com/gorilla/websocket"
+	proto "github.com/microhq/stream-srv/proto/stream"
 )
 
-func StreamCall(w http.ResponseWriter, r *http.Request) {
-	// decode the incoming request as json
-	var request map[string]interface{}
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		http.Error(w, err.Error(), 500)
-		return
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		CheckOrigin:     func(r *http.Request) bool { return true },
 	}
 
-	// call the backend service
-	streamClient := stream.NewStreamService("go.micro.srv.stream", client.DefaultClient)
-	rsp, err := streamClient.Call(context.TODO(), &stream.Request{
-		Name: request["name"].(string),
-	})
+	Client proto.StreamService
+)
+
+func videoClient(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
 	}
 
-	// we want to augment the response
-	response := map[string]interface{}{
-		"msg": rsp.Msg,
-		"ref": time.Now().UnixNano(),
+	// subscribe to the stream
+	stream, err := Client.Subscribe(context.TODO(), &proto.SubscribeRequest{
+		Id: r.Form.Get("id"),
+	})
+
+	if err != nil {
+		fmt.Println("Subscribe error for", r.Form.Get("id"), err.Error())
+		return
 	}
 
-	// encode and write the response as json
-	if err := json.NewEncoder(w).Encode(response); err != nil {
+	for {
+		msg, err := stream.Recv()
+		// stream ended
+		if err == io.EOF {
+			return
+		}
+		// some other error
+		if err != nil {
+			fmt.Println("Stream receive error for", r.Form.Get("id"), err.Error())
+			return
+		}
+		// write the data to the websocket
+		if err := conn.WriteMessage(websocket.TextMessage, msg.Data); err != nil {
+			fmt.Println("Write message error for", r.Form.Get("id"), err.Error())
+			return
+		}
+	}
+}
+
+func StreamVideo(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	id := r.Form.Get("id")
+	typ := r.Form.Get("type")
+
+	if len(id) == 0 {
+		http.Error(w, "id not set", 500)
+		return
+	}
+
+	// client
+	if typ == "client" {
+		fmt.Println("Subscribing to stream", id)
+		videoClient(w, r)
+		return
+	}
+
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
 		http.Error(w, err.Error(), 500)
 		return
+	}
+
+	fmt.Println("Publishing stream", id)
+
+	// create a video stream
+	_, err = Client.Create(context.TODO(), &proto.CreateRequest{
+		Id: id,
+	})
+	if err != nil {
+		fmt.Println("Error creating stream", id, err.Error())
+		return
+	}
+
+	// send stream
+	stream, err := Client.Publish(context.TODO())
+	if err != nil {
+		fmt.Println("Error publishing stream", id, err.Error())
+		return
+	}
+
+	// send loop
+	for {
+		// read from websocket
+		_, d, err := conn.ReadMessage()
+		if err != nil {
+			http.Error(w, err.Error(), 500)
+			fmt.Println("Error reading message", id, err.Error())
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		// send to server
+		if err := stream.Send(&proto.Message{
+			Id:   id,
+			Data: d,
+		}); err != nil {
+			fmt.Println("Error sending message", id, err.Error())
+			return
+		}
 	}
 }
--- main.go
+++ main.go
@@ -1,33 +1,39 @@
 package main
 
 import (
-        "github.com/micro/go-log"
+	"github.com/micro/go-log"
 	"net/http"
 
-        "github.com/micro/go-web"
-        "github.com/microhq/stream-web/handler"
+	"github.com/micro/go-web"
+	proto "github.com/microhq/stream-srv/proto/stream"
+	"github.com/microhq/stream-web/handler"
 )
 
 func main() {
 	// create new web service
-        service := web.NewService(
-                web.Name("go.micro.web.stream"),
-                web.Version("latest"),
-        )
+	service := web.NewService(
+		web.Name("go.micro.web.stream"),
+		web.Version("latest"),
+	)
 
 	// initialise service
-        if err := service.Init(); err != nil {
-                log.Fatal(err)
-        }
+	if err := service.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// setup client
+	client := service.Options().Service.Client()
+
+	handler.Client = proto.NewStreamService("go.micro.srv.stream", client)
 
 	// register html handler
 	service.Handle("/", http.FileServer(http.Dir("html")))
 
 	// register call handler
-	service.HandleFunc("/stream/call", handler.StreamCall)
+	service.HandleFunc("/video", handler.StreamVideo)
 
 	// run service
-        if err := service.Run(); err != nil {
-                log.Fatal(err)
-        }
+	if err := service.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
